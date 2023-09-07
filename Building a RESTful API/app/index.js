d6b4bfb3442805bfd46871553ea710a21d61a9a6
Josue Martinez <jmartinezolivares1@gmail.com>
2023-09-07 08:27:04 -0600

    Parsing HTTP methods

--- Building a RESTful API/app/index.js
+++ Building a RESTful API/app/index.js
@@ -17,16 +17,19 @@ const server = http.createServer( (req, res) => {
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+    // Get the HTTP method
+    const method = req.method.toLowerCase();
+
     // Send the response
     res.end('Hello world\n');
 
     // Log the request path
-    console.log(`Request received on path: ${trimmedPath}`);
+    console.log(`Request received on path: ${trimmedPath} with method: ${method}`);
 
 
 });
 
 // Start the server, and have it listen on port 3000
-server.listen(3001, () => {
-    console.log('The server is listening on port 3001 now');
+server.listen(3000, () => {
+    console.log('The server is listening on port 3000 now');
 })
