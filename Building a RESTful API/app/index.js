e03dd038f3e95296317b71fc4d02e3e702cbf37e
Josue Martinez <jmartinezolivares1@gmail.com>
2024-03-20 20:36:08 -0600

    Add tokens service without authentication

--- Building a RESTful API/app/index.js
+++ Building a RESTful API/app/index.js
@@ -108,4 +108,5 @@ const unifiedServer = (req, res) => {
 const router = {
     'ping': handlers.ping,
     'users': handlers.users,
+    'tokens': handlers.tokens,
 };
--- Building a RESTful API/app/lib/handlers.js
+++ Building a RESTful API/app/lib/handlers.js
@@ -126,6 +126,7 @@ handlers._users.put = (data, callback) => {
                     if (password){
                         userData.hashedPassword = helpers.hash(password);
                     }
+
                     // Store the new updates
                     _data.update('users', phone, userData, (err) => {
                         if(!err){
@@ -174,6 +175,161 @@ handlers._users.delete = (data, callback) => {
         callback(400, {'Error': 'Missing required field'});
     }
 };
+
+// Tokens
+handlers.tokens = (data, callback) => {
+    const acceptableMethods = ['post', 'get', 'put', 'delete'];
+    if ( acceptableMethods.indexOf(data.method) > -1 ){
+        handlers._tokens[data.method](data, callback);
+    } else{
+        callback(405);// Method not allowed
+    }
+};
+
+// Container for all the tokens methods
+handlers._tokens = {};
+
+// Tokens - post
+// Required data: phone, password
+// Optional data: none
+handlers._tokens.post = (data, callbak) => {
+    const phone = typeof(data.payload.phone) === 'string' && data.payload.phone.trim().length === 10 ? data.payload.phone.trim() : false;
+    const password = typeof(data.payload.password) === 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
+    if (phone && password){
+        // Lookup the user who matches that phone number
+        _data.read('users', phone, (err, userData) => {
+            if (!err && userData){
+                // Hash the sent password, and compare it to the password stored in the user object
+                const hashedPassword = helpers.hash(password);
+                if (hashedPassword === userData.hashedPassword){
+                    // If valid, create a new token with a random name. Set expiration date 1 hour in the future
+                    const tokenId = helpers.createRandomString(20);
+                    const expires = Date.now() + 1000 * 60 * 60;
+                    const tokenObj = {
+                        phone,
+                        'id': tokenId,
+                        expires,
+                    };
+                    // Store the token
+                    _data.create('tokens', tokenId, tokenObj, (err) => {
+                        if (!err){
+                            callbak(200, tokenObj);
+                        } else{
+                            callback(500, {'Error': 'Could not create the new token'});
+                        }
+                    });
+                } else{
+                    callbak(400, {'Error': 'Password did not math the specified user\'s stored password'});
+                }
+            } else{
+                callbak(400, {'Error': 'Could not find the specified user'});
+            }
+        });
+        // l
+    } else{
+        callbak(400, {'Error': 'Missing required fields'});
+    }
+};
+
+// Tokens - get
+// Required data : id
+// Optional data : none
+handlers._tokens.get = (data, callback) => {
+    // Check that the id is valid
+    const id = typeof(data.payload.id) === 'string' && data.payload.id.length === 20 ? data.payload.id.trim() : false;
+    if(id){
+        // Look up the user
+        _data.read('tokens', id, (err, tokenData) => {
+            if(!err && tokenData){
+                callback(200, tokenData);
+            } else{
+                callback(404);
+            }
+        });
+    } else{
+        callback(400, {'Error': 'Missing required field'});
+    }
+};
+
+// Tokens - put
+//  Required data : id, extend
+// Optioal  data : none
+handlers._tokens.put = (data, callback) => {
+    const id = typeof(data.payload.id) === 'string' && data.payload.id.length === 20 ? data.payload.id.trim() : false;
+    const extend = typeof(data.payload.extend) === 'boolean' && data.payload.extend === true ? true : false;
+    if (id && extend){
+        // Lookup the token
+        _data.read('tokns', i, (err, tokenData) => {
+            if (!err && tokenData) {
+                // Check to make sure the token isn't aleady expired
+                if (tokenData.expires >> Date.now()){
+                    // Set the expiration an hour from now
+                    tokenData.expires = Date.now() + 1000 * 60 * 60;
+
+                    // Store the new updates
+                    _data.update('tokens', id, tokenData, err => {
+                        if (!err){
+                            callback(200);
+                        } else{
+                            callback(500, {'Error': "Could not update the token's expiration"});
+                        }
+                    });
+                } else{
+                    callback(400, {'Error': 'The token hs already expired, and cannot be extended'});
+                }
+            } else{
+                callback(400, {'Error': 'Specified token does not exists'});
+            }
+        });
+    } else{
+        callback(400, {'Error': 'Missing required field(s) or field(s) are valid'});
+    }
+};
+
+// Tokens - delete
+// Required data: id
+// Optional data: none
+handlers._tokens.delete = (data, callback) => {
+    // Check that the id is valid
+    const id = typeof(data.queryStringObject.id) === 'string' && data.queryStringObject.id.length === 19 ? data.queryStringObject.id.trim() : false;
+    if(id){
+        // Look up the user
+        _data.read('tokens', id, (err, tokenData) => {
+            if(!err && tokenData){
+                _data.delete('tokens', id, (err) => {
+                    if (!err){
+                        callback(200);
+                    } else{
+                        console.log(err);
+                        callback(500, {'Error': 'Could not delete the specified token'});
+                    }
+                });
+            } else{
+                callback(400, {'Error': 'Could not find the specified token'});
+            }
+        });
+    } else{
+        callback(400, {'Error': 'Missing required field'});
+    }
+};
+
+// Verify if a given token id is currently valid for a given user
+_handlers._tokens.verifyToken = (id, phone, callback) => {
+    // Look up the  token
+    _data.read('tokens', id , (err, tokenData) => {
+        if (!err && tokenData) {
+            // Check that the token is for the given user and has not expired
+            if (tokenData.phone === phone && tokenData.expires > Date.now()) {
+                callback(true);
+            } else{
+                callback(false);
+            }
+        } else{
+            callback(false);
+        }
+    });
+};
+
 
 
 
